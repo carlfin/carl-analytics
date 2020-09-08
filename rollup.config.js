a61c28f9b10a27ef25d7b252513c78cda2ee40e6
Richard Gorman <richardgorman@outlook.com>
2020-09-08 20:50:53 +0200

    Added a standalone build for browsers

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,28 @@
+import { terser } from 'rollup-plugin-terser';
+import builtins from 'rollup-plugin-node-builtins';
+
 const babel = require('rollup-plugin-babel');
 const resolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 const replace = require('@rollup/plugin-replace');
 
 const env = process.env.NODE_ENV;
+const plugins = [
+  builtins(),
+  babel({
+    exclude: 'node_modules/**',
+    presets: ['@babel/env', '@babel/preset-react'],
+  }),
+  resolve({
+    browser: true,
+    preferBuiltins: false,
+    extensions: ['.mjs', '.js', '.jsx', '.json', '.node'],
+  }),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(env),
+  }),
+  commonjs(),
+];
 
 module.exports = [
   {
@@ -13,24 +32,26 @@ module.exports = [
     },
     output: [
       {
-        chunkFileNames: 'chunk-[hash].js',
-        dir: 'dist',
+        dir: 'cjs',
         format: 'cjs',
       },
     ],
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/env', '@babel/preset-react'],
-      }),
-      resolve({
-        preferBuiltins: false,
-        extensions: ['.mjs', '.js', '.jsx', '.json', '.node'],
-      }),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify(env),
-      }),
-      commonjs(),
+    plugins,
+  },
+  {
+    input: {
+      index: 'src/index.js',
+    },
+    output: [
+      {
+        dir: 'browser',
+        format: 'iife',
+        name: 'carl.analytics',
+        plugins: [
+          terser(),
+        ],
+      },
     ],
+    plugins,
   },
 ];
